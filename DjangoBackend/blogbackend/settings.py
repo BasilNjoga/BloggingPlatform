37455ff2a6d0685f5327962b9b0c39709904ea5e
Basil Njoga <Basilnjoga@gmail.com>
2024-02-02 22:40:59 +0300

    created database and created database models

--- DjangoBackend/blogbackend/settings.py
+++ DjangoBackend/blogbackend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "myblog"
+    "myblog.apps.MyblogConfig"
 ]
 
 MIDDLEWARE = [
--- DjangoBackend/myblog/models.py
+++ DjangoBackend/myblog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.postgres import fields as postgresFields
 
-# Create your models here.
+
+
+class UserBlog(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    body = models.TextField(default="")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    created_by = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.title + " - " + self.created_by
