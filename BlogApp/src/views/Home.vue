52d9296a2b68500413ac8d889d042e8299f345b6
Basil Njoga <Basilnjoga@gmail.com>
2024-02-04 23:27:41 +0300

    completed api calls using axios

--- BlogApp/src/views/Home.vue
+++ BlogApp/src/views/Home.vue
@@ -1,25 +1,41 @@
-<script setup>
+<script >
 import Header from '../components/Header.vue'
 import store from '../store.js'
-import posts from '../blogsapi'
+import axios from 'axios'
+import { ref } from 'vue'
 
-const getWordsNumber = (str) => (str.split(' ').length)
+
+export default {
+
+    setup () {
+
+        const posts = ref('')
+
+        axios.get('http://127.0.0.1:8000/blogapp/blogs')
+            .then(response => {
+                posts.value = response.data
+            })
+
+        return {
+            posts
+        }
+    }
+    
+}
+//const getWordsNumber = (str) => (str.split(' ').length)
 
 </script>
 
 <template>
     <div class="Home">
         <main class="container mx-auto " >
-            {{ posts }}
-            <!-- <div class="PostItem border border-slate-700 mb-2 p-4 rounded-lg cursor-pointer"
-             v-for="item, itemIndex in store.posts" :key="itemIndex"
-             @click="$router.push(`/post/${item.id}`)">
-            
-            <h1 class="text-slate-900 text-3xl font-bold my-2"> 
-                {{ item.title }}
-            </h1>
-            <p class="text-xl my-2"> {{ getWordsNumber(item.description)  }} words.</p> 
-            </div> -->
+    
+            <div class="PostItem border border-slate-700 mb-2 p-4 rounded-lg cursor-pointer"
+            v-for="item, itemIndex in posts" :key="itemIndex"
+            @click="$router.push(`/post/${item.id}`)">
+                <h1 class="text-slate-900 text-3xl font-bold my-2"> {{  item.title  }}</h1>
+                <p class="text-xl my-2"> {{ item.body }}</p>
+            </div>
         </main>
     </div>
 
--- BlogApp/src/views/Post.vue
+++ BlogApp/src/views/Post.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useRoute } from 'vue-router';
+//import posts from '../views'
 import store from '../store'
 
 const route = useRoute()
